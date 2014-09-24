7558615106fe32367d05d7427f16351d4c73ceca
ludo1026 <ludovic.chaboud@sogeti.com>
2014-09-24 18:01:48 +0200

    SQL types

--- src/main/java/com/restlet/sqlimport/export/TypeConverter.java
+++ src/main/java/com/restlet/sqlimport/export/TypeConverter.java
@@ -15,6 +15,7 @@ public class TypeConverter {
 	public static final String TYPE_FLOAT = "Float";
 	public static final String TYPE_INTEGER = "Integer";
 	public static final String TYPE_BOOLEAN = "Boolean";
+	public static final String TYPE_DATE = "Date";
 
 	private final Report report;
 
@@ -33,8 +34,13 @@ public class TypeConverter {
 	public void convertTypeFromSQLToEntityStore(final Database database) {
 		for(final Table table : database.getTables()) {
 			for(final Column column : table.getColumnByNames().values()) {
-				column.setConvertedType(
-						convertTypeFromSQLToEntityStore(column.getType()));
+				if(column.getType() == null) {
+					column.setConvertedType("");
+				} else {
+					final String sqlType = column.getType().trim().toUpperCase();
+					column.setConvertedType(
+							convertTypeFromSQLToEntityStore(sqlType));
+				}
 			}
 		}
 	}
@@ -44,22 +50,222 @@ public class TypeConverter {
 	 * @param sqlType SQL type
 	 * @return Entity store type
 	 */
-	public String convertTypeFromSQLToEntityStore(final String sqlType) {
+	public String convertTypeFromSQLToEntityStore(String sqlType) {
 		if(sqlType == null) {
 			return null;
 		}
-		if(isString(sqlType)) {
+
+		// format sql type for string comparaison
+		sqlType = sqlType.trim().toUpperCase();
+
+		if(sqlType.equals("BFILE") || (sqlType.indexOf("BFILE ") == 0)) {
 			return TYPE_STRING;
 		}
-		if(isFloat(sqlType)) {
-			return TYPE_FLOAT;
-		}
-		if(isInteger(sqlType)) {
+		if(sqlType.equals("BIGINT") || (sqlType.indexOf("BIGINT ") == 0)) {
 			return TYPE_INTEGER;
 		}
-		if(isBoolean(sqlType)) {
+		if(sqlType.equals("BIGSERIAL") || (sqlType.indexOf("BIGSERIAL ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("BINARY") || (sqlType.indexOf("BINARY ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("BINARY_DOUBLE") || (sqlType.indexOf("BINARY_DOUBLE ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("BINARY_FLOAT") || (sqlType.indexOf("BINARY_FLOAT ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("BIT VARYING") || (sqlType.indexOf("BIT VARYING ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("BIT") || (sqlType.indexOf("BIT ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("BLOB") || (sqlType.indexOf("BLOB ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("BOOL") || (sqlType.indexOf("BOOL ") == 0)) {
 			return TYPE_BOOLEAN;
 		}
+		if(sqlType.equals("BOOLEAN") || (sqlType.indexOf("BOOLEAN ") == 0)) {
+			return TYPE_BOOLEAN;
+		}
+		if(sqlType.equals("BYTEA") || (sqlType.indexOf("BYTEA ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("CHAR") || (sqlType.indexOf("CHAR ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("CHARACTER VARYING") || (sqlType.indexOf("CHARACTER VARYING ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("CHARACTER") || (sqlType.indexOf("CHARACTER ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("CLOB") || (sqlType.indexOf("CLOB ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("DATE") || (sqlType.indexOf("DATE ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("DATETIME") || (sqlType.indexOf("DATETIME ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("DEC") || (sqlType.indexOf("DEC ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("DECIMAL") || (sqlType.indexOf("DECIMAL ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("DOUBLE PRECISION") || (sqlType.indexOf("DOUBLE PRECISION ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("DOUBLE") || (sqlType.indexOf("DOUBLE ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("ENUM") || (sqlType.indexOf("ENUM ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("FIXED") || (sqlType.indexOf("FIXED ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("FLOAT") || (sqlType.indexOf("FLOAT ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("INT") || (sqlType.indexOf("INT ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("INTEGER") || (sqlType.indexOf("INTEGER ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("INTERVAL DAY") || (sqlType.indexOf("INTERVAL DAY ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("INTERVAL YEAR") || (sqlType.indexOf("INTERVAL YEAR ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("INTERVAL") || (sqlType.indexOf("INTERVAL ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("LONG RAW") || (sqlType.indexOf("LONG RAW ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("LONG") || (sqlType.indexOf("LONG ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("LONGBLOB") || (sqlType.indexOf("LONGBLOB ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("LONGTEXT") || (sqlType.indexOf("LONGTEXT ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("MEDIUMBLOB") || (sqlType.indexOf("MEDIUMBLOB ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("MEDIUMINT") || (sqlType.indexOf("MEDIUMINT ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("MEDIUMTEXT") || (sqlType.indexOf("MEDIUMTEXT ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("MONEY") || (sqlType.indexOf("MONEY ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("NATIONAL CHARACTER VARYING") || (sqlType.indexOf("NATIONAL CHARACTER VARYING ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("NATIONAL CHARACTER") || (sqlType.indexOf("NATIONAL CHARACTER ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("NCHAR") || (sqlType.indexOf("NCHAR ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("NCLOB") || (sqlType.indexOf("NCLOB ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("NUMBER") || (sqlType.indexOf("NUMBER ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("NUMERIC") || (sqlType.indexOf("NUMERIC ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("NVARCHAR") || (sqlType.indexOf("NVARCHAR ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("NVARCHAR2") || (sqlType.indexOf("NVARCHAR2 ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("RAW") || (sqlType.indexOf("RAW ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("REAL") || (sqlType.indexOf("REAL ") == 0)) {
+			return TYPE_FLOAT;
+		}
+		if(sqlType.equals("ROWID") || (sqlType.indexOf("ROWID ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("SERIAL") || (sqlType.indexOf("SERIAL ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("SET") || (sqlType.indexOf("SET ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("SMALLINT") || (sqlType.indexOf("SMALLINT ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("SMALLSERIAL") || (sqlType.indexOf("SMALLSERIAL ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("TEXT") || (sqlType.indexOf("TEXT ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("TIME WITH TIME ZONE") || (sqlType.indexOf("TIME WITH TIME ZONE ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("TIME") || (sqlType.indexOf("TIME ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("TIMESTAMP WITH TIME ZONE") || (sqlType.indexOf("TIMESTAMP WITH TIME ZONE ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("TIMESTAMP") || (sqlType.indexOf("TIMESTAMP ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("TIMESTAMPTZ") || (sqlType.indexOf("TIMESTAMPTZ ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("TIMETZ") || (sqlType.indexOf("TIMETZ ") == 0)) {
+			return TYPE_DATE;
+		}
+		if(sqlType.equals("TINYBLOB") || (sqlType.indexOf("TINYBLOB ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("TINYINT") || (sqlType.indexOf("TINYINT ") == 0)) {
+			return TYPE_INTEGER;
+		}
+		if(sqlType.equals("TINYTEXT") || (sqlType.indexOf("TINYTEXT ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("UROWID") || (sqlType.indexOf("UROWID ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("UUID") || (sqlType.indexOf("UUID ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("VARBINARY") || (sqlType.indexOf("VARBINARY ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("VARCHAR") || (sqlType.indexOf("VARCHAR ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("VARCHAR2") || (sqlType.indexOf("VARCHAR2 ") == 0)) {
+			return TYPE_STRING;
+		}
+		if(sqlType.equals("YEAR") || (sqlType.indexOf("YEAR ") == 0)) {
+			return TYPE_INTEGER;
+		}
+
 		report.addMessage(ReportLineStatus.UNKNOWN_SQL_TYPE, sqlType);
 		return TYPE_STRING;
 	}
@@ -70,21 +276,109 @@ public class TypeConverter {
 	 * @return boolean
 	 */
 	public boolean isString(final String sqlType) {
-		if("CHAR".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BFILE") || (sqlType.indexOf("BFILE ") == 0)) {
 			return true;
 		}
-		if("VARCHAR".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BINARY") || (sqlType.indexOf("BINARY ") == 0)) {
 			return true;
 		}
-		if("VARCHAR2".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BIT") || (sqlType.indexOf("BIT ") == 0)) {
 			return true;
 		}
-		if("CHARACTER".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BIT VARYING") || (sqlType.indexOf("BIT VARYING ") == 0)) {
 			return true;
 		}
-		if("character varying".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BLOB") || (sqlType.indexOf("BLOB ") == 0)) {
 			return true;
 		}
+		if(sqlType.equals("BYTEA") || (sqlType.indexOf("BYTEA ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("CHAR") || (sqlType.indexOf("CHAR ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("CHARACTER") || (sqlType.indexOf("CHARACTER ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("CHARACTER VARYING") || (sqlType.indexOf("CHARACTER VARYING ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("CLOB") || (sqlType.indexOf("CLOB ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("ENUM") || (sqlType.indexOf("ENUM ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("INTERVAL") || (sqlType.indexOf("INTERVAL ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("LONG RAW") || (sqlType.indexOf("LONG RAW ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("LONGBLOB") || (sqlType.indexOf("LONGBLOB ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("LONGTEXT") || (sqlType.indexOf("LONGTEXT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("MEDIUMBLOB") || (sqlType.indexOf("MEDIUMBLOB ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("MEDIUMTEXT") || (sqlType.indexOf("MEDIUMTEXT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NATIONAL CHARACTER") || (sqlType.indexOf("NATIONAL CHARACTER ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NATIONAL CHARACTER VARYING") || (sqlType.indexOf("NATIONAL CHARACTER VARYING ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NCHAR") || (sqlType.indexOf("NCHAR ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NCLOB") || (sqlType.indexOf("NCLOB ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NVARCHAR") || (sqlType.indexOf("NVARCHAR ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NVARCHAR2") || (sqlType.indexOf("NVARCHAR2 ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("RAW") || (sqlType.indexOf("RAW ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("ROWID") || (sqlType.indexOf("ROWID ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("SET") || (sqlType.indexOf("SET ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TEXT") || (sqlType.indexOf("TEXT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TINYBLOB") || (sqlType.indexOf("TINYBLOB ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TINYTEXT") || (sqlType.indexOf("TINYTEXT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("UROWID") || (sqlType.indexOf("UROWID ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("UUID") || (sqlType.indexOf("UUID ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("VARBINARY") || (sqlType.indexOf("VARBINARY ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("VARCHAR") || (sqlType.indexOf("VARCHAR ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("VARCHAR2") || (sqlType.indexOf("VARCHAR2 ") == 0)) {
+			return true;
+		}
+
 		return false;
 	}
 
@@ -94,12 +388,43 @@ public class TypeConverter {
 	 * @return boolean
 	 */
 	public boolean isFloat(final String sqlType) {
-		if("DECIMAL".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BINARY_DOUBLE") || (sqlType.indexOf("BINARY_DOUBLE ") == 0)) {
 			return true;
 		}
-		if("NUMBER".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BINARY_FLOAT") || (sqlType.indexOf("BINARY_FLOAT ") == 0)) {
 			return true;
 		}
+		if(sqlType.equals("DEC") || (sqlType.indexOf("DEC ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("DECIMAL") || (sqlType.indexOf("DECIMAL ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("DOUBLE") || (sqlType.indexOf("DOUBLE ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("DOUBLE PRECISION") || (sqlType.indexOf("DOUBLE PRECISION ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("FIXED") || (sqlType.indexOf("FIXED ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("FLOAT") || (sqlType.indexOf("FLOAT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("MONEY") || (sqlType.indexOf("MONEY ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NUMBER") || (sqlType.indexOf("NUMBER ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("NUMERIC") || (sqlType.indexOf("NUMERIC ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("REAL") || (sqlType.indexOf("REAL ") == 0)) {
+			return true;
+		}
+
 		return false;
 	}
 
@@ -109,12 +434,46 @@ public class TypeConverter {
 	 * @return boolean
 	 */
 	public boolean isInteger(final String sqlType) {
-		if("INT".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BIGINT") || (sqlType.indexOf("BIGINT ") == 0)) {
 			return true;
 		}
-		if("INTEGER".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BIGSERIAL") || (sqlType.indexOf("BIGSERIAL ") == 0)) {
 			return true;
 		}
+		if(sqlType.equals("INT") || (sqlType.indexOf("INT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("INTEGER") || (sqlType.indexOf("INTEGER ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("INTERVAL DAY") || (sqlType.indexOf("INTERVAL DAY ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("INTERVAL YEAR") || (sqlType.indexOf("INTERVAL YEAR ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("LONG") || (sqlType.indexOf("LONG ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("MEDIUMINT") || (sqlType.indexOf("MEDIUMINT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("SERIAL") || (sqlType.indexOf("SERIAL ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("SMALLINT") || (sqlType.indexOf("SMALLINT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("SMALLSERIAL") || (sqlType.indexOf("SMALLSERIAL ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TINYINT") || (sqlType.indexOf("TINYINT ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("YEAR") || (sqlType.indexOf("YEAR ") == 0)) {
+			return true;
+		}
+
 		return false;
 	}
 
@@ -124,12 +483,45 @@ public class TypeConverter {
 	 * @return boolean
 	 */
 	public boolean isBoolean(final String sqlType) {
-		if("BOOL".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BOOL") || (sqlType.indexOf("BOOL ") == 0)) {
 			return true;
 		}
-		if("BOOLEAN".equalsIgnoreCase(sqlType)) {
+		if(sqlType.equals("BOOLEAN") || (sqlType.indexOf("BOOLEAN ") == 0)) {
 			return true;
 		}
+
+		return false;
+	}
+
+	/**
+	 * 
+	 */
+	public boolean isDate(final String sqlType) {
+		if(sqlType.equals("DATE") || (sqlType.indexOf("DATE ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("DATETIME") || (sqlType.indexOf("DATETIME ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TIME") || (sqlType.indexOf("TIME ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TIME WITH TIME ZONE") || (sqlType.indexOf("TIME WITH TIME ZONE ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TIMESTAMP") || (sqlType.indexOf("TIMESTAMP ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TIMESTAMP WITH TIME ZONE") || (sqlType.indexOf("TIMESTAMP WITH TIME ZONE ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TIMESTAMPTZ") || (sqlType.indexOf("TIMESTAMPTZ ") == 0)) {
+			return true;
+		}
+		if(sqlType.equals("TIMETZ") || (sqlType.indexOf("TIMETZ ") == 0)) {
+			return true;
+		}
+
 		return false;
 	}
 
--- src/test/java/com/restlet/sqlimport/export/TypeConverterTest.java
+++ src/test/java/com/restlet/sqlimport/export/TypeConverterTest.java
@@ -27,24 +27,75 @@ public class TypeConverterTest {
 		assertEquals(ReportLineStatus.UNKNOWN_SQL_TYPE, report.getReportLines().get(0).getReportLineStatus());
 		assertEquals("UNKNOWN", report.getReportLines().get(0).getMessage());
 
-		// String
-		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore("VARCHAR"));
-		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore("VARCHAR2"));
-		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore("CHAR"));
-		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore("character"));
-		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore("character varying"));
-
-		// Float
-		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore("NUMBER"));
-		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore("DECIMAL"));
-
-		// Integer
-		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore("INTEGER"));
-		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore("INT"));
-
-		// Boolean
-		assertEquals("Boolean", typeConverter.convertTypeFromSQLToEntityStore("BOOLEAN"));
-		assertEquals("Boolean", typeConverter.convertTypeFromSQLToEntityStore("BOOL"));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" bfile "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" bigint "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" bigserial "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" binary "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" binary_double "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" binary_float "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" bit "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" bit varying "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" blob "));
+		assertEquals("Boolean", typeConverter.convertTypeFromSQLToEntityStore(" bool "));
+		assertEquals("Boolean", typeConverter.convertTypeFromSQLToEntityStore(" boolean "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" bytea "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" char "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" character "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" character varying "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" clob "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" date "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" datetime "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" dec "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" decimal "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" double "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" double precision "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" enum "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" fixed "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" float "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" int "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" integer "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" interval "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" interval day [day_precision] to second [fractional seconds] "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" interval year [year_precision] to month "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" long "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" long raw "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" longblob "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" longtext "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" mediumblob "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" mediumint "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" mediumtext "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" money "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" national character "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" national character varying "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" nchar "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" nclob "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" number "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" numeric "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" nvarchar "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" nvarchar2 "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" raw "));
+		assertEquals("Float", typeConverter.convertTypeFromSQLToEntityStore(" real "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" rowid "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" serial "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" set "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" smallint "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" smallserial "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" text "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" time "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" time with time zone "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" timestamp "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" timestamp with time zone "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" timestamptz "));
+		assertEquals("Date", typeConverter.convertTypeFromSQLToEntityStore(" timetz "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" tinyblob "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" tinyint "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" tinytext "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" urowid "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" uuid "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" varbinary "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" varchar "));
+		assertEquals("String", typeConverter.convertTypeFromSQLToEntityStore(" varchar2 "));
+		assertEquals("Integer", typeConverter.convertTypeFromSQLToEntityStore(" year "));
 
 		assertEquals(1, report.getReportLines().size());
 	}
